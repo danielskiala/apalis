5ae00c3cde2a7feea9c9a4101e483e727a3b5630
Daniel's Kiala <danielskiala@gmail.com>
2025-11-23 23:25:46 +0100

    Premiere version

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NavComponent } from '../../components/nav/nav.component';
 
 @Component({
@@ -8,15 +8,19 @@ import { NavComponent } from '../../components/nav/nav.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   @ViewChild('video') video: ElementRef<HTMLVideoElement>;
   status_video: boolean;
+  constructor() {}
+  ngOnInit() {
+  }
 
   startVideoBanner() {
     if (!this.video.nativeElement.paused) {
       this.video.nativeElement.pause();
       this.status_video = true;
     } else {
+      this.status_video = false;
       this.video.nativeElement.play();
     }
   }
